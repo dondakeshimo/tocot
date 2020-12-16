7b6aef1c8ef52e08b1cdac70b7b529e9b65b6d2e
dondakeshimo <went.went.takkun135@gmail.com>
2020-12-17 00:57:28 +0900

    feat: add docstring

--- src/tocot/main.py
+++ src/tocot/main.py
@@ -1,3 +1,8 @@
+"""main.
+
+main is controller for cli.
+
+"""
 import typing
 
 import click
@@ -6,6 +11,12 @@ from .toc_builder import TOCBuilder
 
 
 def validate_level(ctx, param, value):
+    """Validate level option.
+
+    validate level option.
+    level is greater than 0, and less than 6.
+
+    """
     level = value
 
     if level <= 0:
@@ -32,12 +43,22 @@ def validate_level(ctx, param, value):
               type=str)
 def cmd(in_file: typing.TextIO, out_file: typing.TextIO, level: int,
         to_embed: str, exclude_symbol: str) -> None:
+    """Cmd.
+
+    cmd is a controller.
+
+    """
     builder = TOCBuilder(in_file, out_file, level, to_embed, exclude_symbol)
     builder.build()
     builder.write()
 
 
 def main():
+    """Vain.
+
+    main is a entrypoint.
+
+    """
     cmd()
 
 
